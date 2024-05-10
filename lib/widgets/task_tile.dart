921f931ee293d1114f034b3dc5fc8ea6ae3bc272
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-05-10 18:22:27 +0100

    Add files via upload

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:riverpod_to_do_app/data/models/task.dart';
+import 'package:riverpod_to_do_app/utils/utils.dart';
 
 class TaskTile extends StatelessWidget {
-  const TaskTile({super.key, required this.task});
+  const TaskTile({super.key, required this.task, this.onCompleted});
 
   final Task task;
+  final Function(bool?)? onCompleted;
 
   @override
   Widget build(BuildContext context) {
+    final style = context.textTheme;
+    final double iconOpacity = task.isCompleted ? 0.3 : 0.5;
+    final backgroundOpacity = task.isCompleted ? 0.1 : 0.3;
+    final textDecoration =
+        task.isCompleted ? TextDecoration.lineThrough : TextDecoration.none;
+    final fontWeight = task.isCompleted ? FontWeight.normal : FontWeight.bold;
+
     return Padding(
       padding: const EdgeInsets.only(
         left: 16,
@@ -21,16 +30,16 @@ class TaskTile extends StatelessWidget {
             padding: EdgeInsets.all(9.0),
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: task.category.color.withOpacity(0.3),
+              color: task.category.color.withOpacity(backgroundOpacity),
               border: Border.all(
                 width: 2,
-                color: task.category.color,
+                color: task.category.color.withOpacity(iconOpacity),
               ),
             ),
             child: Center(
               child: Icon(
                 task.category.icon,
-                color: task.category.color,
+                color: task.category.color.withOpacity(iconOpacity),
               ),
             ),
           ),
@@ -39,14 +48,26 @@ class TaskTile extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(task.title),
-                Text(task.time),
+                Text(
+                  task.title,
+                  style: style.titleMedium?.copyWith(
+                    decoration: textDecoration,
+                    fontSize: 20,
+                    fontWeight: fontWeight,
+                  ),
+                ),
+                Text(
+                  task.time,
+                  style: style.titleMedium?.copyWith(
+                    decoration: textDecoration,
+                  ),
+                ),
               ],
             ),
           ),
           Checkbox(
             value: task.isCompleted,
-            onChanged: (value) {},
+            onChanged: onCompleted,
           ),
         ],
       ),
